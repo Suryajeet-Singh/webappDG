918d26923c9a35afab020596b7e2d8777b2aac73
Suryajeet-Singh <suryasingh8922@gmail.com>
2025-06-28 12:48:31 +0530

    DecimalNumber

---
+++ src/TataElexis1DecimalNumber.java
@@ -0,0 +1,24 @@
+public class TataElexis1DecimalNumber {
+    public static void main(String[] args) {
+        String str = "12476581.3245";
+        StringBuilder str3 = new StringBuilder();
+        StringBuilder str4 = new StringBuilder();
+        String[] str2  = str.split("\\.");
+        for(int i = 0; i< str2.length-1;i++){
+            char[] ch = str2[i].toCharArray();
+            for(int j = 0 ; j< ch.length-1;j++){
+                str3.append(ch[j]);
+                j++;
+            }
+        }
+        for(int k= 1; k< str2.length;k++){
+            char[] ch = str2[k].toCharArray();
+            for(int l = 0 ; l< ch.length-1;l++){
+                str4.append(ch[l]);
+                l++;
+            }
+        }
+        System.out.print(str3+"."+str4);
+    }
+
+}
